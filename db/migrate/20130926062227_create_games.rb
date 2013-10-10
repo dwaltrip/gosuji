3df359221d98c0803aff4f3b48487187eb0c8b6b
Daniel Waltrip <dwaltrip77@gmail.com>
2013-10-09 20:00:04 -0700

    Update database schema for Users and Games tables. Clean up and simplify db migrations, as there is no production data yet.

--- db/migrate/20130926062227_create_games.rb
+++ db/migrate/20130926062227_create_games.rb
@@ -1,12 +1,19 @@
 class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
-      t.references :player1, :index => true, :null => false
-      t.references :player2, :index => true, :null => false
+      t.references :black_player, :index => true
+      t.references :white_player, :index => true
+      t.references :creator, :null => false
+
+      t.string :description
+      t.integer :board_size, :limit => 2
+      t.integer :handicap, :limit => 2
+      t.integer :komi, :limit => 2
+      t.string :rule_set
+      t.string :time_settings
 
       t.integer :status, :limit => 2
-      t.integer :board_size, :limit => 2
-      t.string :time_settings
+      t.integer :type, :limit => 2
 
       t.timestamp :started_at, :finished_at
       t.timestamps
--- db/migrate/20130926092312_create_users.rb
+++ db/migrate/20130926092312_create_users.rb
@@ -1,10 +1,17 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username, :email
+      t.string :username, :null => false
+      t.string :password_digest
+      t.string :email
+      t.integer :status, :limit => 2
+      t.string :rank
       t.text :account_settings
 
       t.timestamps
     end
+
+    add_index :users, :username, :unique => true
+    add_index :users, :email
   end
 end
