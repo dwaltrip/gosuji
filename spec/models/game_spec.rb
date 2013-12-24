e32e2a5138f1a4cfb438ac73ec093def175c39c0
Daniel Waltrip <dwaltrip77@gmail.com>
2013-12-23 22:32:04 -0800

    Add game description length validation test

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Game do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:game) { Game.new }
+
+  it "has a description with no more than 40 characters" do
+    game.should be_valid
+    game.description = "super duper more than 40 character long descriptive piece of text"
+    game.should_not be_valid
+  end
+
 end
