28b2dee1dcfaa0dc16d9a32beb342de6dc5bb160
Daniel Waltrip <dwaltrip77@gmail.com>
2014-01-25 19:57:41 -0800

    Enhance and clean up integration/feature specs
    Make integration tests are more thorough (provide better coverage)
    Refactor a fair amount as well -- more DRY and readable
    Quite painfully overcame many obscure Postgres/Capybara errors >_<
    Also do small tweak to .gitignore

--- spec/features/gameplay_actions_spec.rb
+++ spec/features/gameplay_actions_spec.rb
@@ -4,36 +4,32 @@ require 'spec_helper'
 feature "gameplay actions" do
 
   scenario "players play several basic moves back and forth" do
-    size = GoApp::MIN_BOARD_SIZE
-    players, game = setup_game_and_sessions(board_size: size)
+    players, game, color_map = setup_game_and_sessions
+    size = game.board_size
 
-    tiles = [
-      "#tile-#{size + 1}",
-      "#tile-#{2 * size - 2}",
-      "#tile-#{4 * size - 2}",
-      "#tile-#{3 * size + 1}"
-    ]
+    tiles = [(size + 1), (2 * size - 2), (4 * size - 2), (3 * size + 1)].map { |n| selector_for_tile(n) }
     stone_counts = { black: 0, white: 0 }
 
     # ensure both players have blank game boards to start
-    in_browsers(players) do |player|
+    as(players) do |player|
       expect(page).to have_blank_tile(count: size**2)
       expect(page).not_to have_stone
     end
 
     # alternate between black and white player, playing a move, and assert browser displays board properly
-    players.cycle(tiles.length) do |current_player, counter|
-      color = game.player_color(current_player)
+    Cycle.new(players).cycle(tiles.length) do |current_player, counter|
+      color = color_map[current_player.id]
       move_num = counter + 1
       stone_counts[color] += 1
       tile = tiles[counter]
 
-      # current player plays next move
-      in_browser(current_player) { find(tile).click }
+      # current player plays next move. expect ajax request
+      as(current_player) do
+        expect{ find(tile).click }.to change{ active_ajax_requests }.by(1)
+      end
 
-      # expect both players to see the updated board
-      in_browsers(players) do
-
+      # expect both players to see the updated game
+      as(players) do
         # loop through old moves and verify they are being displayed properly
         Cycle.new([:black, :white]).cycle(counter) do |prev_color, inner_counter|
           prev_tile = tiles[inner_counter]
@@ -44,84 +40,152 @@ feature "gameplay actions" do
         expect(page).to have_stone(color: color, count: stone_counts[color])
         expect(page).to have_stone(highlighted: false, count: move_num - 1)
         expect(page).to have_blank_tile(count: size**2 - move_num)
-        expect(page).to have_content("Move #{counter + 1}")
+        expect(page).to have_status_message(move_number: counter + 1)
+      end
+
+      # after playing a move, nothing should happen when current_player clicks a tile
+      as(current_player) do
+        expect(page).not_to have_clickable_tile
+
+        all_tiles(size).each do |tile|
+          expect{ find(tile).click }.to change{ active_ajax_requests }.by(0) # expect no ajax request
+        end
       end
     end
   end
 
 
   scenario "player passes their turn" do
-    size = GoApp::MIN_BOARD_SIZE
-    players, game = setup_game_and_sessions(board_size: size)
-    first_player = game.active_player
+    players, game, color_map = setup_game_and_sessions
+    size = game.board_size
+    player1, player2 = players
+    tile = selector_for_tile(1)
 
-    in_browsers(players) do |player|
-      if player == first_player
-        click_button "Pass"
+    # first player makes a move
+    as(player1) do
+      find(tile).click
+      expect(page).not_to have_button("Pass")
+    end
+
+    # second player passes their turn. expect ajax request
+    as(player2) { expect{ click_button "Pass" }.to change{ active_ajax_requests }.by(1) }
+
+    as(players) do |player|
+      expect(page).to have_status_message(pass: true, color: color_map[player2.id])
+      expect(page).to have_status_message(move_number: 2)
+      expect(find(tile)).to have_stone(highlighted: false) # last played stone shouldn't be highlighted
+    end
+
+    # it is now player1's turn, so turn actions should be enabled
+    as(player1) do
+      expect(page).to have_button("Pass")
+      expect(page).to have_clickable_tile(count: size**2 - 1)
+    end
+
+    # player2 should not be able to perform turn actions
+    as(player2) do
+      expect(page).not_to have_button("Pass")
+      expect(page).not_to have_clickable_tile
+
+      all_tiles(size).each do |tile|
+        expect{ find(tile).click }.to change{ active_ajax_requests }.by(0) # expect no ajax request
+      end
+    end
+  end
+
+
+  scenario "player plays move, then requests undo, and other player approves the undo" do
+    players, game, color_map = setup_game_and_sessions
+    size = game.board_size
+
+    player1, player2 = players
+    opponent = { player1.id => player2, player2.id => player1 }
+    tiles = [selector_for_tile(3), selector_for_tile(11), selector_for_tile(19)]
+
+    # cycle through tile array, alternating moves between black player & white player
+    # undo should be disabled before a player makes their first move, after that enabled
+    Cycle.new(players).cycle(tiles.length) do |player, counter|
+      as(player) do
+        expect(page).not_to have_button("Undo") if counter <= 1
+        find(tiles[counter]).click
+        expect(page).to have_button("Undo")
       end
     end
 
-    in_browsers(players) do |player|
-      expect(page).to have_content("#{game.player_color(first_player).capitalize[0]} pass")
-      expect(page).to have_content("Move 1")
+    as(players) { expect(page).to have_status_message(move_number: tiles.length) }
+
+    # player1 has had two turns, player2 has had one turn
+    # both now request undo, with player2 going first
+    # undo for player2 reverts two moves (move 2 & 3) -- undo for player1 reverts one move (move 1)
+    players.reverse_each do |requester|
+      approver = opponent[requester.id]
+      reverted_tile_count = if (requester == player2) then 2 else 1 end
+
+      # make the undo request. expect an ajax request
+      as(requester) { expect{ click_button "Undo" }.to change{ active_ajax_requests }.by(1) }
+
+      # other player should see popup modal allowing the approve/denial of the undo request
+      as(approver) do
+        expect(undo_modal).to have_content("has requested an undo")
+        expect(undo_modal).to have_button("Yes")
+        expect(undo_modal).to have_button("No")
+
+        # approve the undo request. expect an ajax request
+        expect{ undo_modal.click_button "Yes" }.to change{ active_ajax_requests }.by(1)
+      end
+
+      # remove undone moves from tiles array and store in reverted_tiles
+      reverted_tiles = Array.new(reverted_tile_count) { tiles.pop }
+
+      # both players should now see the updated board, reverted to before requester's last move
+      as(players) do |player|
+        expect(page).to have_status_message(move_number: tiles.length)
+        expect(page).to have_stone(count: tiles.length)
+        expect(page).to have_blank_tile(count: size**2 - tiles.length)
+        reverted_tiles.each { |tile| expect(find(tile)).to have_blank_tile }
+
+        # if there are any stones still on board, check that the most recent one is highlighted
+        if tiles.length > 0
+          expect(find(tiles[-1])).to have_stone(color: color_map[approver.id], highlighted: true)
+        end
+      end
+
+      # approver should not be able to perform turn actions
+      as(approver) do
+        expect(page).not_to have_button("Pass")
+        expect(page).not_to have_clickable_tile
+
+        all_tiles(size).each do |tile|
+          expect{ find(tile).click }.to change{ active_ajax_requests }.by(0) # expect no ajax request
+        end
+      end
+
+      # requester should have turn actions enabled
+      as(requester) do
+        expect(page).to have_clickable_tile(count: size**2 - tiles.length)
+        expect(page).to have_button("Pass")
+      end
     end
+
+    # undo should not be possible once again, as all moves have been reverted
+    as(players) { expect(page).not_to have_button("Undo") }
   end
-
-
-  scenario "player plays move, then requests undo, and other player grants the undo" do
-    size = GoApp::MIN_BOARD_SIZE
-    players, game = setup_game_and_sessions(board_size: size)
-    first_player = game.active_player    # this is the player who will grant the undo
-    second_player = game.inactive_player # this is the player who will request an undo
-
-    first_tile = "#tile-3"
-    tile_to_undo = "#tile-13"
-    tiles = { first_player.id => first_tile, second_player.id => tile_to_undo }
-
-    # each player makes a move
-    # undo should be disabled before a player makes their first move, and then enabled
-    in_browsers(players) do |player|
-      expect(page).not_to have_button("Undo")
-      find(tiles[player.id]).click
-      expect(page).to have_button("Undo")
-    end
-
-    # second player requests undo
-    in_browser(second_player) do
-      click_button "Undo"
-    end
-
-    # first player should see popup allowing approve/deny the request for undo
-    in_browser(first_player) do
-      expect(page).to have_content("has requested an undo")
-      expect(find("#undo-approval-form")).to have_button("Yes")
-      expect(find("#undo-approval-form")).to have_button("No")
-
-      # approve the undo request
-      find("#undo-approval-form").click_button "Yes"
-    end
-
-    # both players should now see the updated board with the most recent move reverted
-    in_browsers(players) do |player|
-      expect(find(first_tile)).to have_stone(color: game.player_color(first_player), highlighted: true)
-      expect(find(tile_to_undo)).to have_blank_tile
-
-      expect(page).to have_stone(count: 1)
-      expect(page).to have_blank_tile(count: size**2 - 1)
-      expect(page).to have_content("Move 1")
-    end
-
-    # undo should not be possible once again, as the only move made by this player was undone already
-    in_browser(second_player) do
-      expect(page).not_to have_button("Undo")
-    end
-  end
-
 end
 
 
 # helper methods to be used as natural rspec matchers, by returning calls to the macther have_selector
 # this gives more useful failure messages, better use of capybara's synching/waiting abilities
+def have_status_message(options)
+  content_string =
+    if options[:move_number]
+      ("Game Start" if (options[:move_number] == 0)) || "Move #{options[:move_number]}"
+    elsif options[:pass]
+      "#{options[:color].capitalize[0]} pass"
+    end
+
+  have_content(content_string)
+end
+
 def have_blank_tile(options={})
   options[:css_class] = ".board_tile"
   have_image("blank_tile", options)
@@ -149,18 +213,33 @@ def have_image(img_src, options={})
   have_selector(selector, options)
 end
 
+def have_clickable_tile(options={})
+  have_selector(".clickable", options)
+end
+
+
+def all_tiles(size)
+  (0...size**2).map { |i| selector_for_tile(i) }
+end
+
+def selector_for_tile(n)
+  "#tile-#{n}"
+end
+
+def undo_modal
+  find("#undo-approval-container")
+end
+
 
 # create game, load game in browser for both players, and make sure websockets are working
-def setup_game_and_sessions(options)
-  players = Cycle.new([
-    create(:user, username: 'player1'),
-    create(:user, username: 'player2')
-  ])
+def setup_game_and_sessions(options={})
+  players = [create(:user, username: 'player1'), create(:user, username: 'player2')]
   board_size = options[:board_size] || GoApp::MIN_BOARD_SIZE
   game = create_active_game(players, board_size=board_size)
+  color_map = Hash[players.map { |player| [player.id, game.player_color(player)] }]
 
   # both players visit site
-  in_browsers(players) do |player|
+  as(players) do |player|
     sign_in_as player, "secret"
     visit game_path(game)
 
@@ -168,7 +247,7 @@ def setup_game_and_sessions(options)
     page.wait_until(5) { page.evaluate_script "socket !== null && socket.protocol !== null" }
   end
 
-  [players, game]
+  [players, game, color_map]
 end
 
 def create_active_game(players, size=GoApp::BOARD_SIZE)
@@ -187,10 +266,15 @@ def sign_in_as(user, password)
   click_button 'Log in'
 end
 
+def as(player_or_players)
+  # use in_browser if just one player, otherwise use in_browsers
+  _method = (:in_browsers if player_or_players.respond_to?(:each)) || :in_browser
+  send(_method.to_sym, player_or_players) { |*args| yield *args }
+end
+
 def in_browsers(players)
-  players.each do |player|
-    Capybara.session_name = player.username
-    yield player
+  players.each_with_index do |player, index|
+    in_browser(player) { yield player, index }
   end
 end
 
@@ -200,26 +284,38 @@ def in_browser(player)
 end
 
 
-# useful for debugging
-def save_page_html(current_page, suffix=nil)
-  time_str = Time.now.strftime('%Y-%m-%d-%H-%M-%S-%L')
+def active_ajax_requests
+  evaluate_script("$.active")
+end
 
-  File.open("#{Rails.root}/log/tmp/capybara/#{time_str}#{suffix.to_s}.html", "w") do |file|
-    file.write(current_page.html)
+# useful for debugging
+def save_page_html(current_page, options={})
+  filepath = "#{tmp_base_path}/#{timestamp}#{options[:suffix]}"
+
+  File.open("#{filepath}.html", "w") do |file|
+    page_html = current_page.html
+    page_html.gsub! "/assets", "assets"
+    file.write(page_html)
+  end
+
+  if options[:screenshot]
+    current_page.save_screenshot("#{filepath}.png", full: true)
   end
 end
 
+def tmp_base_path
+  "#{Rails.root}/log/tmp/capybara"
+end
+def timestamp
+  Time.now.strftime('%Y-%m-%d-%H-%M-%S-%L')
+end
 
-# yes, creating this class for use in the feature specs is slight overkill
-# but I got to learn about fibers, it is elegant to use, and it was fun to make =)
-class Cycle
-  def initialize(enum)
-    @enum = enum
-  end
 
-  # repeats @enum.each until until 'n' total iterations
-  # differs from built-in Enumerable.cycle in that 'n' represents number of items accessed,
-  # not number of times we loop through the enumerable
+# allows us to cycle through an array 'n' times, on the item level
+# the built-in Enumerable.cycle just repeates the entire array 'n' times, not what I want
+# Ex: Cycle.new([1, 2, 3]).cycle(5) -> 1, 2, 3, 1, 2
+# as an aside, Fibers are cool
+class Cycle < Array
   def cycle(n)
     restart
     n.times { |i| yield @fiber.resume, i }
@@ -227,13 +323,7 @@ class Cycle
 
   def restart
     @fiber = Fiber.new do
-      @enum.each { |item| Fiber.yield item } while true
+      self.each { |item| Fiber.yield item } while true
     end
   end
-
-  def each
-    @enum.each { |item| yield item }
-  end
-
-  def to_a; @enum; end
 end
